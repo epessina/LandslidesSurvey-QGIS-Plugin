1cb6d813af3f00f3d82377c5e61599f1712ba1de
Edoardo Pessina <edoardopessina@yahoo.it>
2019-08-24 07:48:54 +0200

    Updated licence

--- landslides_survey_dialog.py
+++ landslides_survey_dialog.py
@@ -18,8 +18,8 @@
 
 /****************************************************************
  *    This program is free software; you can redistribute it    *
- *    and/or modify it under the terms of the MIT License       *
- *    (https://choosealicense.com/licenses/mit/)                *
+ *    and/or modify it under the terms of the GNU GPLv3         *
+ *    License https://choosealicense.com/licenses/gpl-3.0/#)    *
  ***************************************************************/
 """
 
